cb4e3d196e14028e69fa1803ce9883cfaaaa6b8a
U-glitsh <itsmosscow@gmail.com>
2024-08-18 12:22:13 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,23 +3,23 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "201210307869"
+global.ownername = " 7rb@lex "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
-global.botname = 'WASI TECH BUG BOT ' //name of the bot
+global.ownernumber = '201210307869'  //creator number
+global.ownername = ' 7@rbalex ' //owner name
+global.botname = '7rb BUG BOT ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'WASI'
+global.author = '7rb'
 
 //console view/theme
 global.themeemoji = '🪀'
-global.wm = "WASI TECH"
+global.wm = "7rb"
 
 //theme link
 global.link = 'https://whatsapp.com'
